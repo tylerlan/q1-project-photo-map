9375bb27b1da57ca425759cb2c508086ca538435
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-05-16 12:21:04 -0700

    Added some basic display to the page

--- brunch-config.js
+++ brunch-config.js
@@ -1,5 +1,11 @@
-// See http://brunch.io for documentation.
-exports.files = {
-  javascripts: {joinTo: 'app.js'},
-  stylesheets: {joinTo: 'app.css'}
+module.exports = {
+  files: {
+    javascripts: { joinTo: 'app.js' },
+    stylesheets: { joinTo: 'app.css' }
+  },
+  plugins: {
+    eslint: {
+      pattern: /app\/.+\.js$/
+    }
+  }
 };
