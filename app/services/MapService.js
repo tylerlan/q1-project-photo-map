3e7fff0ef265d3010870eccfdcae05eb404305df
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-05-16 23:58:12 -0700

    Added Google Maps JS API key and basic init function

--- app/services/MapService.js
+++ app/services/MapService.js
@@ -1,6 +1,18 @@
+const KEY = 'AIzaSyDtet_-9zOt0miA0G0mlaeldeICJvlrBVI';
+
 module.exports = class MapService {
   constructor () {
+
   }
+
+  initMap(latitude, longitude) {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: latitude, lng: longitude},
+          zoom: 8
+        });
+
+        return map;
+      }
 
 
 }
